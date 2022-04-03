149437dac8062e588d267119b54abe40c853722c
Rasmus Nitsche <nitschedev@gmail.com>
2022-04-03 19:24:36 +0200

    updated api controller

--- server/controllers/api.controller.js
+++ server/controllers/api.controller.js
@@ -1,4 +1,12 @@
+const User = require('../models/user')
 
 exports.main = (req,res) => {
-    res.send('Hello World')
+    User.find({ name: 'Test User' }, (error,result) => {
+        if (error) return console.log(error)
+        res.json({
+            status: 200,
+            ok: true,
+            body: result.length > 0 ? result : null
+        })
+    })
 }
