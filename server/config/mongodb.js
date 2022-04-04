c0da907fc300112706b81afdb0cb4419e61434b5
Rasmus Nitsche <nitschedev@gmail.com>
2022-04-04 19:45:43 +0200

    production & mongodb setup

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 const mongo_uri = process.env.MONGODB || ''
 
 const connectDB = (mongo_uri) => {
-    if (mongo_uri === '') return console.log(`No MongoDB credentials was found in .env file!`)
+    if (mongo_uri === '') return console.log(
+        `No MongoDB credentials was found in .env file! -> MongoDB NOT Connected`
+        )
     mongoose.connect(mongo_uri || '').catch(error => {
         console.log(error)
     })
--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const { connectDB } = require('./config/mongodb')
 const app = express()
+const path = require('path')
 
 //Middlewere
 app.use(express.json())
@@ -12,6 +13,15 @@ connectDB(process.env.MONGODB)
 
 //Routes
 app.use('/api', require('./routes/api.route'))
+
+//Production Setup
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+    //catch all(*) routes after the api routes and serve the client index.html
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
+    })
+}
 
 //Start Server
 const port = process.env.PORT
