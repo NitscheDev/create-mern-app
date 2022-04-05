c153d79dfc1c231b34a847383b3531f9b9759742
Rasmus Nitsche <nitschedev@gmail.com>
2022-04-05 18:49:13 +0200

    update server & build client

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose')
-const mongo_uri = process.env.MONGODB || ''
 
 const connectDB = (mongo_uri) => {
     if (mongo_uri === '') return console.log(
         `No MongoDB credentials was found in .env file! -> MongoDB NOT Connected`
         )
-    mongoose.connect(mongo_uri || '').catch(error => {
-        console.log(error)
-    })
-    console.log('MongoDB Connected...')
+    mongoose.connect(mongo_uri || '')
+        .then(() => console.log('MongoDB Connected...'))
+        .catch(error => console.log(error))
 }
 
 module.exports = { connectDB }
